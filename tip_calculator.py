38596ea8e7ea68fcf16f9427968f4246339bfd3f
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-23 08:56:47 -0500

    updated readme and python file

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,9 @@
-# print welecome to the tip calculator
-print('Welcome to the tip calculator')
+# create a variable name and assign the string Ms. Johnson
+# print how was your meal and here's the bill no rush with customer name
+customer_name = ('Ms. Johnson')
+print(f'How was your meal and here is the bill no rush {customer_name}. ', '\n')
 
-# get the total bill amount from the customer to calculate
+# get the total bill amount for the customer to calculate tip
 # next create a variable name bill equals to a input as a float with text enter the total bill amount
 bill = float(input('Total bill amount: '))
 
@@ -11,7 +13,14 @@ tip = int(input('Enter tip percentage (%): '))
 # create a variable peolpe equals an input as a integer with text 'split by how many'
 people = int(input('Split by how many: '))
 
-# print the variables 'bill', 'tip', and people on seperate lines
-print('Bill:', bill) 
-print('Tip:', tip)
-print('People:', people)
+bill_before_tip = bill * (tip/100 + 1)
+
+tax = bill * .1
+
+final_bill = round(bill_before_tip + tax, 3)
+
+split = round(final_bill / people)
+
+print(f'Total bill to pay: ${final_bill}', '\n')
+
+print(f'Each person owes ${split} towards dinner with tax and tip')
