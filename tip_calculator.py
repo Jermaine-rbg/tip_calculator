f3fdb15769566868c4c2499c1c0de4e8af8bf5b8
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-03-05 21:34:56 -0500

    updated comments

--- tip_calculator.py
+++ tip_calculator.py
@@ -4,7 +4,7 @@ customer_name = ('Ms. Johnson')
 print(f'How was your meal and here is the bill no rush {customer_name}. ', '\n')
 
 # get the total bill amount for the customer to calculate tip
-# next create a variable name bill equals to a input as a float with text enter the total bill amount
+# created a variable name bill equals to a input as a float with text enter the total bill amount
 bill = float(input('Total bill amount: '))
 
 # created a tip variable tip equals an input as a integer with the text 'enter tip percentage' using the (%) - modulus
@@ -13,7 +13,7 @@ tip = int(input('Enter tip percentage (%): '))
 # created a people variable peolpe equals an input as a integer with text 'split by how many'
 people = int(input('Split by how many: '))
 
-# created a bill before tip variable equals what the bill is including the tip 
+# created a bill before tip variable equals what the bill is including the tip inputed
 bill_before_tip = bill * (tip/100 + 1)
 
 # created a tax variable equals the bill multiplied by a 10% tax thats repersented by .1
@@ -22,7 +22,7 @@ tax = bill * .1
 # created a final bill variable equals to the rounded bill total plus tax
 final_bill = round(bill_before_tip + tax, 3)
 
-# created a split variable equals to the rounded final bill divided by the number of people
+# created a split variable equals to the rounded final bill divided by the number of people inputed
 split = round(final_bill / people)
 
 # printing the total bill to pay with tax and tip
